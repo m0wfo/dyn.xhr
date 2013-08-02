41b6989d3c52a8ab24f8be7c95ef76efee438445
Chris Mowforth <chris@mowforth.com>
2013-08-02 14:52:11 +0100

    testing impl

--- dyn.xhr.js
+++ dyn.xhr.js
@@ -1,5 +1,88 @@
 var XMLHttpRequest = function() {
-  print("hi")
+
+  var _channel = java.nio.channels.AsynchronousSocketChannel.open();
+  var _method = null;
+  var _address = null;
+  var _path = null;
+  var _headers = {"User-Agent": "dyn.xhr/0.0.1"};
+  var _encoder = java.nio.charset.Charset.forName("US-ASCII").newEncoder();
+
+  _encoder.reset();
+
+  this.readyState = 0;
+
+  this.onreadystatechange = function() {};
+
+  this.open = function(method, url, async) {
+    _method = method;
+    var uri = new java.net.URI(url);
+    _address = new java.net.InetSocketAddress(uri.getHost(), uri.getPort());
+    _path = uri.getPath();
+  }
+
+  this.setRequestHeader = function(key, value) {
+    _headers[key] = value;
+  }
+
+  var _buildRequest = function(data) {
+    var request = _method + " " + _path + " HTTP/1.1\r\n";
+    _headers["Content-Length"] = data.length;
+    _headers["Accept"] = "*/*";
+    _headers["Host"] = "localhost:8080";
+    for (var k in _headers) {
+      request += k + ": " + _headers[k] + "\r\n";
+    }
+    request += ("\r\n" + data);
+    print(request);
+    return request;
+  }
+
+  var _parseResponse = function(data) {
+    print(data.split("\r\n")[0].split("\s"));
+  }
+
+  this.status = 0;
+
+  var that = this;
+
+  this.send = function(data) {
+    var req = _buildRequest(data);
+    var cb = java.nio.CharBuffer.wrap(req);
+    var bb = _encoder.encode(cb);
+    _channel.connect(_address, null, new java.nio.channels.CompletionHandler({
+      completed: function(result, attachment) {
+        that.readyState = 1;
+        that.onreadystatechange();
+        print("connected");
+        _channel.write(bb, null, new java.nio.channels.CompletionHandler({
+          completed: function(result, attachment) {
+            print("written!");
+            var bb = java.nio.ByteBuffer.allocate(4096);
+            _channel.read(bb, null, new java.nio.channels.CompletionHandler({
+              completed: function(result, attachment) {
+                var str = new java.lang.String(bb.array());
+                var resp = _parseResponse(str);
+                _channel.close();
+              }
+            }));
+          },
+          failed: function(err, attachment) {
+            print("err");
+            _channel.close();
+          }
+        }));
+      },
+      failed: function(err, attachment) {
+        print(err);
+        _channel.close();
+      }
+    }));
+  }
 }
 
+xhr = new XMLHttpRequest();
+xhr.open("POST", "http://localhost:8080/logs/bbe3a2f5-5170-4a02-af1b-24622692af27", true);
+xhr.setRequestHeader("Content-Type", "text/plain");
+xhr.send("some data");
 
+java.lang.Thread.sleep(1000);
